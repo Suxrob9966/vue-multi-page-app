2f013a3a3e8b5011ee7856bb126ec75d47befc03
Suxrob9966 <rabbimovich9966@gmail.com>
2023-05-14 13:31:42 -0500

    organizing router files

--- src/main.js
+++ src/main.js
@@ -1,68 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' }, //redirect to teams if there is slash nothing
-    {
-      name: 'teams',
-      path: '/teams',
-      meta: { needsAuth: true },
-      components: { default: TeamsList, footer: TeamsFooter },
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true,
-        }, // everything after : is routeParams,
-      ],
-    }, // our-domain.com/teams => TeamsList
-    {
-      path: '/users',
-      components: { default: UsersList, footer: UsersFooter },
-      beforeEnter(to, from, next) {
-        console.log('Users beforeEnter');
-        console.log(to, from);
-        next();
-      },
-    }, // our-domain.com/users => UsersList
-    { path: '/:notFound(.*)', component: NotFound }, // if not existing route is entered
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(_, _2, savedPosition) {
-    // console.log(to, from, savedPosition);
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach(function (to, from, next) {
-  // console.log('Global before each');
-  // console.log(to, from);
-  if (to.meta.needsAuth) {
-    console.log('Needs auth!');
-    next();
-  } else {
-    next();
-  }
-  next(); // next() is used to perform action before navigating to another route
-});
-
-router.afterEach(function (to, from) {
-  // good for sending analytics
-});
+import router from './router.js';
 
 const app = createApp(App);
 
---
+++ src/router.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './pages/NotFound.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' }, //redirect to teams if there is slash nothing
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        }, // everything after : is routeParams,
+      ],
+    }, // our-domain.com/teams => TeamsList
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        console.log('Users beforeEnter');
+        console.log(to, from);
+        next();
+      },
+    }, // our-domain.com/users => UsersList
+    { path: '/:notFound(.*)', component: NotFound }, // if not existing route is entered
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(_, _2, savedPosition) {
+    // console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+router.beforeEach(function (to, from, next) {
+  // console.log('Global before each');
+  // console.log(to, from);
+  if (to.meta.needsAuth) {
+    console.log('Needs auth!');
+    next();
+  } else {
+    next();
+  }
+  next(); // next() is used to perform action before navigating to another route
+});
+
+router.afterEach(function (to, from) {
+  // good for sending analytics
+});
+
+export default router;
